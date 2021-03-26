b8c2b91b189ee68ea808c1881d81602ea498309c
agerlach <agerlach@inovex.de>
2021-03-26 22:44:36 +0100

    add training script

--- tools/train_robust_misc.py
+++ tools/train_robust_misc.py
@@ -85,11 +85,6 @@ def main(args):
     trainer = DefaultTrainer(cfg)
     trainer.resume_or_load(resume=False)
 
-    if args.test:
-        cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
-        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # testing threshold for model
-        cfg.DATASETS.TEST = ("robust_misc_val")
-
     return trainer.train()
 
 
